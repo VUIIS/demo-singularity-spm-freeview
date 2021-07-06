d94b99c66d7764f831c87091fd3ca61da41e8378
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-07-06 17:42:41 -0500

    bash

--- src/pipeline_entrypoint.sh
+++ src/pipeline_entrypoint.sh
@@ -57,4 +57,4 @@ done
 # Now that we have all the inputs stored in environment variables, call the
 # main pipeline. We run it in xvfb so that we have a virtual display available.
 xvfb-run -n $(($$ + 99)) -s '-screen 0 1600x1200x24 -ac +extension GLX' \
-    pipeline_main.sh
+    bash pipeline_main.sh
